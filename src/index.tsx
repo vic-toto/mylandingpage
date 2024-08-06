7f1fc490c82fb0259ec2b8a48c2bd4e6e90ae72c
tech3380722 <tech@deckx.app>
2024-08-06 18:56:46 +0200

    brat

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,3 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css'; // Import global styles if needed
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
